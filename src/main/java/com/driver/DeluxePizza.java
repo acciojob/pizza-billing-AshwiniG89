83fd68f64213586e15561122c582d20dca1cc2ea
Ashwini <pruashu@gmail.com>
2023-01-18 16:07:56 +0530

    First Solution

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,14 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+    @Override
+    public int getPrice() {
+        return super.getPrice();
+    }
+
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
     }
+
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -8,27 +8,49 @@ public class Pizza {
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
-    }
+        if(isVeg==true){
+            int vegPizza =300;
+            this.price= price+vegPizza;
+           // System.out.println("Base Price Of The Pizza: "+vegPizza);
+        }else{
+            int nonVegPizza =400;
+            this.price= price+nonVegPizza;
+            //System.out.println("Base Price Of The Pizza: "+nonVegPizza);
+        }
+     }
 
     public int getPrice(){
+        System.out.print("Base Price Of The Pizza: ");
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        int cheese=80;
+        this.price= price+cheese;
+       // System.out.println("Extra Cheese Price = "+cheese);
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        int vegTopping = 70;
+        int nonVegTopping = 120;
+        if(isVeg==true){
+            this.price=price+vegTopping;
+            System.out.println("Extra Topping For Veg Pizza = "+vegTopping);
+        }else{
+            this.price=price+120;
+            System.out.println("Extra Topping For Non-Veg Pizza = "+nonVegTopping);
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        int bag=20;
+        this.price=price+bag;
+        System.out.println("Paper bag Price = "+bag);
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+       // System.out.println();
+
+        return "Total Price: "+price;
     }
 }
